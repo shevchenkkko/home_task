88c3fdc52c650a712049b8397a48fea9f223691a
shevchenkkko <sevcenkoz825@gmail.com>
2025-10-26 14:27:09 +0200

    division function added

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ def calc_sum(a,b):
 
 def multiplication(a,b):
     return a*b
+
+
+def division(a,b):
+    try:
+        result = a/b
+        return result
+    except ZeroDivisionError:
+        return "You can't devide by 0"
+        
